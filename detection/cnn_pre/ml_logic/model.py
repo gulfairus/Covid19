4cb2d3f8a1faf2b1efb840d1b27d674dfbec37b0
gulfairus kairedenova <gulfairuskairedenova@gmail.com>
2024-03-17 02:32:48 +0500

    metrics

--- detection/cnn_pre/ml_logic/model.py
+++ detection/cnn_pre/ml_logic/model.py
@@ -66,7 +66,7 @@ def compile_model(model: Model, learning_rate) -> Model:
     Compile the Neural Network
     """
     optimizer = optimizers.Adam(learning_rate=learning_rate)
-    model.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=["accuracy", "F1score"])
+    model.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=["accuracy", "f1_score"])
 
     print("✅ Model compiled")
 
