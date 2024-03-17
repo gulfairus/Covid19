15b385966928b20fe718a086d0baa539c19aa0f8
gulfairus kairedenova <gulfairuskairedenova@gmail.com>
2024-03-17 11:24:39 +0500

    preprocess_input

--- detection/cnn_pre/ml_logic/preprocessor.py
+++ detection/cnn_pre/ml_logic/preprocessor.py
@@ -12,7 +12,7 @@ import requests
 from io import BytesIO
 import random
 from detection.params import *
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, preprocess_input
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 from tensorflow.keras.applications.efficientnet import preprocess_input
 import time
