6d5e07aac855e8dd0fac010fa62b5e8d38992cbb
gulfairus kairedenova <gulfairuskairedenova@gmail.com>
2024-03-18 04:28:43 +0500

    detection/xgboost/ml_logic/

---
+++ detection/xgboost/ml_logic/model.py
@@ -0,0 +1,118 @@
+import numpy as np
+import time
+
+from colorama import Fore, Style
+from typing import Tuple
+
+# Timing the TF import
+print(Fore.BLUE + "\nLoading TensorFlow..." + Style.RESET_ALL)
+start = time.perf_counter()
+
+from tensorflow import keras
+from keras import Model, Sequential, layers, regularizers, optimizers
+from keras.callbacks import EarlyStopping
+from tensorflow.keras.models import Sequential
+import tensorflow
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, BatchNormalization
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Model, load_model
+#from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+
+end = time.perf_counter()
+print(f"\n✅ TensorFlow loaded ({round(end - start, 2)}s)")
+
+
+
+def initialize_model(input_shape: tuple) -> Model:
+    """
+    Initialize the Neural Network with random weights
+    """
+
+    # it = Iterator(["file_0.svm", "file_1.svm", "file_2.svm"])
+    # Xy = xgboost.DMatrix(it)
+
+    # # The ``approx`` also work, but with low performance. GPU implementation is different from CPU.
+    # # as noted in following sections.
+    # booster = xgboost.train({"tree_method": "hist"}, Xy)
+
+
+    return model
+
+#Compile the CNN
+def compile_model(model: Model, learning_rate) -> Model:
+    """
+    Compile the Neural Network
+    """
+    optimizer = optimizers.Adam(learning_rate=learning_rate)
+    model.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"])
+
+    print("✅ Model compiled")
+
+    return model
+
+def train_model(
+        model: Model,
+        train_data,
+        batch_size,
+        patience,
+        validation_data=None,
+        epochs=None) -> Tuple[Model, dict]:
+    """
+    Fit the model and return a tuple (fitted_model, history)
+    """
+    print(Fore.BLUE + "\nTraining model..." + Style.RESET_ALL)
+
+    es = EarlyStopping(
+        monitor="val_loss",
+        patience=patience,
+        restore_best_weights=True,
+        verbose=1
+    )
+
+    #steps = len(train_names)//batch_size
+
+    history = model.fit(
+        train_data,
+        validation_data=validation_data,
+        epochs=epochs,
+        batch_size=batch_size,
+        callbacks=[es],
+        verbose=1
+    )
+
+
+    print(f"✅ Model trained ")
+
+    return model, history
+
+
+def evaluate_model(
+        model: Model,
+        test_data,
+        batch_size
+    ) -> Tuple[Model, dict]:
+    """
+    Evaluate trained model performance on the dataset
+    """
+
+    print(Fore.BLUE + f"\nEvaluating model ..." + Style.RESET_ALL)
+
+    if model is None:
+        print(f"\n❌ No model to evaluate")
+        return None
+
+    metrics = model.evaluate(
+        test_data = test_data,
+        batch_size=batch_size,
+        verbose=0,
+        # callbacks=None,
+        return_dict=True
+    )
+
+    loss = metrics["loss"]
+    accuracy = metrics["accuracy"]
+
+    print(f"✅ Model evaluated, Accuracy: {round(accuracy, 2)}")
+
+    return metrics
---
+++ detection/xgboost/ml_logic/preprocessor.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import os
+from skimage.transform import resize
+from skimage.io import imread
+import numpy as np
+from google.cloud import storage
+import requests
+from io import BytesIO
+import random
+from detection.params import *
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+import time
+import pickle
+
+
+def preprocess_data():
+
+    # generate training,testing and validation batches
+    train_dir = TRAIN_DATA_PATH
+    test_dir = TEST_DATA_PATH
+
+    Categories=['COVID19','NORMAL', 'OPACITY', 'PNEUMONIA']
+    flat_data_arr=[] #input array
+    target_arr=[] #output array
+    datadir='IMAGES/'
+    #path which contains all the categories of images
+    for i in Categories:
+
+        print(f'loading... category : {i}')
+        path=os.path.join(train_dir,i)
+        for img in os.listdir(path):
+            img_array=imread(os.path.join(path,img))
+            img_resized=resize(img_array,(150,150,3))
+            flat_data_arr.append(img_resized.flatten())
+            target_arr.append(Categories.index(i))
+        print(f'loaded category:{i} successfully')
+    flat_data=np.array(flat_data_arr)
+    target=np.array(target_arr)
+
+    return flat_data.head()
+
+
+
+
+    # dgen_train = ImageDataGenerator(#samplewise_center=True,
+    #                                 #samplewise_std_normalization=True,
+    #                                 rescale = 1./255,
+    #                                 validation_split=0.2,
+    #                                 #rotation_range=20,
+    #                                 shear_range=0.2,
+    #                                 zoom_range = 0.2,
+    #                                 horizontal_flip = False)
+    # dgen_validation = ImageDataGenerator(rescale = 1./255)
+    # dgen_test = ImageDataGenerator(rescale=1./255)
+
+    # train_generator = dgen_train.flow_from_directory(train_dir,
+    #                                                 target_size=(150,150),
+    #                                                 subset = "training",
+    #                                                 batch_size = 32,
+    #                                                 class_mode = "categorical")
+
+    # validation_generator = dgen_train.flow_from_directory(train_dir,
+    #                                                 target_size=(150,150),
+    #                                                 subset = "validation",
+    #                                                 batch_size = 32,
+    #                                                 class_mode = "categorical")
+
+    # test_generator = dgen_test.flow_from_directory(test_dir,
+    #                                                 target_size=(150,150),
+    #                                                 batch_size = 32,
+    #                                                 class_mode = "categorical")
+
+
+    # return train_generator, validation_generator, test_generator
