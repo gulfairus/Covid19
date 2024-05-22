c5d730abd44519f1cf579a21bfc6cc6399f5b81e
gulfairus kairedenova <gulfairuskairedenova@gmail.com>
2024-05-22 09:24:38 +0500

    preprocessor

--- detection/cnn_pre/ml_logic/preprocessor.py
+++ detection/cnn_pre/ml_logic/preprocessor.py
@@ -1,7 +1,3 @@
-"""
-Preprocessing data using generators.
-"""
-
 import pandas as pd
 import os
 # from skimage.transform import resize
@@ -14,72 +10,60 @@ import random
 from detection.params import *
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image_dataset_from_directory
-from tensorflow.keras.applications.efficientnet import preprocess_input
+from PIL import Image
+import tensorflow as tf
+from tensorflow.keras.utils import img_to_array
 import time
 import pickle
+from itertools import chain
 
 
 def preprocess_data():
-    """
-    The function make generations for training, testing and validation by global variables.
-    Images are rescaled and augmentaded (zoom and horizontal flip)
-    """
 
-    # Define directories by global variabels:
-    # train_dir = TRAIN_DATA_PATH_CLOUD
-    # test_dir = TEST_DATA_PATH_CLOUD
+    # generate training,testing and validation batches
 
-    train_dir_normal = TRAIN_DATA_PATH
-    train_dir_no_normal = TRAIN_DATA_PATH
-    test_dir = TEST_DATA_PATH
+    # train_dir = TRAIN_DATA_PATH
+    # test_dir = TEST_DATA_PATH
+    # val_dir = VAL_DATA_PATH
 
-    # Create a generator with augmentation for training and validation:
-    dgen_train_normal = ImageDataGenerator(
-                                    rescale = 1./255,
-                                    validation_split=0.2,
-                                    #shear_range=0.2,
-                                    #zoom_range = 0.2,
-                                    #horizontal_flip = False,
-                                    #preprocessing_function = preprocess_input
-                                    )
-    dgen_train_others = ImageDataGenerator(
-                                    rescale = 1./255,
-                                    validation_split=0.2,
+    train_dir = TRAIN_DATA_PATH_CLOUD
+    test_dir = TEST_DATA_PATH_CLOUD
+    val_dir = VAL_DATA_PATH_CLOUD
+
+    # def random_crop(image):
+    #     height, width = image.shape[:2]
+    #     random_array = np.random.random(size=4);
+    #     w = int((width*0.5) * (1+random_array[0]*0.5))
+    #     h = int((height*0.5) * (1+random_array[1]*0.5))
+    #     x = int(random_array[2] * (width-w))
+    #     y = int(random_array[3] * (height-h))
+
+    #     image_crop = image[y:h+y, x:w+x, 0:3]
+    #     image_crop = image.resize(image_crop, image.shape)
+    #     return image_crop
+
+    #dgen_train_norm = ImageDataGenerator(rescale = 1./255)
+    dgen_train = ImageDataGenerator(rescale = 1./255,
                                     shear_range=0.2,
                                     zoom_range = 0.2,
-                                    #horizontal_flip = False,
-                                    )
+                                    channel_shift_range = 20)
+    dgen_validation = ImageDataGenerator(rescale = 1./255)
+    dgen_test = ImageDataGenerator(rescale=1./255)
 
-    # datagen = ImageDataGenerator(
-    #     featurewise_center = False,
-    #     featurewise_std_normalization = False,
-    #     rotation_range = 10,
-    #     width_shift_range = 0.1,
-    #     height_shift_range = 0.1,
-    #     horizontal_flip = True,
-    #     zoom_range = (0.8, 1.2),
-    #     )
-
-#     model_2.add(layers.RandomFlip("horizontal"))
-# model_2.add(layers.RandomZoom(0.1))
-# model_2.add(layers.RandomTranslation(0.2, 0.2))
-# model_2.add(layers.RandomRotation(0.1))
-
-    # Create a generator without augmentation for test:
-    #dgen_test = ImageDataGenerator(rescale=1./255)
-    dgen_test = ImageDataGenerator(rescale = 1./255)
-
-    # Make generators by directories:
-    # The classes wiil be the subdirectories
     train_generator = dgen_train.flow_from_directory(train_dir,
                                                     target_size=(150,150),
                                                     subset = "training",
                                                     batch_size = 32,
                                                     class_mode = "categorical")
 
-    validation_generator = dgen_train.flow_from_directory(train_dir,
+    # train_generator_other = dgen_train_other.flow_from_directory(train_dir_other,
+    #                                                 target_size=(150,150),
+    #                                                 subset = "training",
+    #                                                 batch_size = 32,
+    #                                                 class_mode = "categorical")
+
+    validation_generator = dgen_validation.flow_from_directory(val_dir,
                                                     target_size=(150,150),
-                                                    subset = "validation",
                                                     batch_size = 32,
                                                     class_mode = "categorical")
 
@@ -88,6 +72,7 @@ def preprocess_data():
                                                     batch_size = 32,
                                                     class_mode = "categorical")
 
+    #train_merged_generator = chain(train_generator_norm, train_generator_other)
 
     return train_generator, validation_generator, test_generator
 
