e4002c0c335d805acb43f90fa4a5427c606f3700
gulfairus kairedenova <gulfairuskairedenova@gmail.com>
2024-05-24 08:07:53 +0500

    valid

--- detection/cnn_pre/ml_logic/preprocessor.py
+++ detection/cnn_pre/ml_logic/preprocessor.py
@@ -43,11 +43,13 @@ def preprocess_data():
     #     return image_crop
 
     #dgen_train_norm = ImageDataGenerator(rescale = 1./255)
+    # dgen_train = ImageDataGenerator(rescale = 1./255,
+    #                                 shear_range=0.2,
+    #                                 zoom_range = 0.2,
+    #                                 #channel_shift_range = 20,
+    #                                 validation_split=0.2)
     dgen_train = ImageDataGenerator(rescale = 1./255,
-                                    shear_range=0.2,
-                                    zoom_range = 0.2,
-                                    #channel_shift_range = 20,
-                                    validation_split=0.2)
+                                   validation_split=0.2)
     # dgen_validation = ImageDataGenerator(rescale = 1./255,
     #                                 shear_range=0.2,
     #                                 zoom_range = 0.2,
